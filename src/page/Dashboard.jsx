e50ee55c93dfec662090970bc2cf615136292ff4
vishalpanchal1024 <vishalpanchal1024@gmail.com>
2025-04-19 12:18:20 +0530

    somethiung

--- src/page/Dashboard.jsx
+++ src/page/Dashboard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Dashboard = () => {
   return (
-    <div className='pt-96  text-white'>hello</div>
+    <div className='pt-96  text-white'>hey</div>
   )
 }
 
---
+++ src/page/Leets.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Leets = () => {
+  return (
+    <div>Leets</div>
+  )
+}
+
+export default Leets
